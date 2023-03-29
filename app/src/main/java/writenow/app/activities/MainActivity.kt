cc68e8dff3a7f1ef97a3255e186e2c938bd2f501
joeyfinkel <61193959+joeyfinkel@users.noreply.github.com>
2023-03-28 20:51:15 -0400

    For merge

--- app/src/main/java/writenow/app/activities/MainActivity.kt
+++ app/src/main/java/writenow/app/activities/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
